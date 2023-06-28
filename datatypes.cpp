89d71d064b27a1091efc7d5fd459089240b1a084
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-28 22:42:33 +0530

    data types and type modifiers

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a = 14;
+    cout<<"sixe of int: "<<sizeof(a)<<endl;
+    float b;
+    cout<<"size of float: "<<sizeof(b)<<endl;
+    char c;
+    cout<<"size of char: "<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size of bool: "<<sizeof(d)<<endl;
+
+    // type modifiers
+    short int e;
+    cout<<"size of si: "<<sizeof(e)<<endl;
+    long int f;
+    cout<<"size of li: "<<sizeof(f)<<endl;
+
+}

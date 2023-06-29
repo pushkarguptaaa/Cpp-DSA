02415fbfac8bf476adfb72b71b5fccba93719763
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-29 15:53:07 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    // for loop
+    // int n;
+    // cin>>n;
+    // int sum=0;
+    // for(int i=1; i<=n; i++){
+    //     sum+=i;
+    // }
+    // cout<<sum<<endl;
+
+
+    //while loop
+    // int n;
+    // cin>>n;
+    // while(n>0){
+    //     cout<<n<<endl;
+    //     cin>>n;
+    // }
+
+    // return 0;
+
+    // do while
+    int n;
+    cin>>n;
+    do{
+        cout<<n<<endl;
+        cin>>n;
+    } while(n>0);
+}

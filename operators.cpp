d0f58fc9cc1e8ee6e5296a1f9d90dc33e7d65c69
Pushkar Gupta <pushkargupta@pushkars-MacBook-Pro.local>
2023-06-30 10:59:33 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int i=1;
+    // i=i++ + ++i;
+    // cout<<i<<endl;
+
+    // int i=1;
+    // int j=2;
+    // int k = i+j+ i++ + j++ + ++i + ++j;
+    // cout<<i<<j<<k<<endl;
+
+    // int i=0;
+    // i=i++ - --i + ++i - i--;
+    // cout<<i<<endl;
+
+    // int i=1,j=2,k=3;
+    // int m=i-- -j-- - k--;
+    // cout<<i<<j<<k<<m<<endl;
+
+    // int i=10,j=20,k;
+    // k= i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+       //10  9      19    20    9    20     10  19
+    // cout<<i<<j<<k<<endl;
+    // 10 20 -20
+
+    
+    return 0;
+}
